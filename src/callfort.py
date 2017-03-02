b8d54c9f62abcb11d1c8eb99b710a1f9a9d654c6
Jessica <jkelly@urban.org>
2017-03-02 14:33:08 -0500

    testing that i can create a signature file :thumbsup: and bare bones python that does nothing, but has comments

---
+++ src/callfort.py
@@ -0,0 +1,13 @@
+#python3
+#
+# Jessica A Kelly
+# March 2017
+# working with f2py.
+# goal is to test a simple fortran function call
+# with a variable that is set in python and then
+# used by the fortran code
+
+import numpy as np
+
+def run():
+    #put some stuff here to run the fortan code
